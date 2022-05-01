1fcde05b9bbd1b0183bf46bab9329f186170dfd4
Marcelo Morais <morais.marcelo@outlook.com>
2022-05-01 18:42:05 -0300

    added index in controler

--- app/Http/Controllers/Dashboard/QueueController.php
+++ app/Http/Controllers/Dashboard/QueueController.php
@@ -3,17 +3,15 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\QueueJob;
 
 class QueueController extends Controller
 {
     public function index()
     {
         return Inertia::render('Dashboard/Queue/Index', [
-            'queue' => [
-                'name' => 'Queue'
-            ]
+            'queues' => QueueJob::all(),
         ]);
     }
 
--- resources/js/Pages/Dashboard/Queue/Index.vue
+++ resources/js/Pages/Dashboard/Queue/Index.vue
@@ -19,8 +19,53 @@ defineProps({
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                    <div class="flex flex-col">
+                    <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                        <div class="overflow-hidden">
+                            <table v-if="queues.length > 0" class="min-w-full">
+                            <thead class="border-b">
+                                <tr>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    #
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    Type
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    Attempts
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                    Created At
+                                </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="queue in queues" :key="queue.id" class="border-b">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                        {{ queue.id }}
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        {{queue.payload.displayName}}
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        {{queue.attempts}}
+                                    </td>
+                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        {{queue.created_at}}
+                                    </td>
+                                </tr>
+                            </tbody>
+                            </table>
 
+                            <p v-else>
+                                No queued job's found.
+                            </p>
 
+                        </div>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
