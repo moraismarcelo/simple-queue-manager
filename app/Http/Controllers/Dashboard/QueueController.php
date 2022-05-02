b6d293683f9fe61af2e2dd787da9b3a7f603607d
Marcelo Morais <morais.marcelo@outlook.com>
2022-05-01 21:28:38 -0300

    added route to run job

--- app/Http/Controllers/Dashboard/QueueController.php
+++ app/Http/Controllers/Dashboard/QueueController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Inertia\Inertia;
 use App\Models\QueueJob;
+use Symfony\Component\HttpFoundation\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class QueueController extends Controller
 {
@@ -14,5 +16,24 @@ class QueueController extends Controller
             'queues' => QueueJob::all(),
         ]);
     }
+
+    public function runJob(Request $request)
+    {
+        $job = QueueJob::find($request->input('id'));
+
+        if($job->update(['queue' => 'urgent'])){
+            Artisan::call('queue:work', [
+                '--queue' => 'urgent',
+                '--tries' => 1,
+                '--stop-when-empty' => true,
+            ]);
+        }
+
+        return response()->json([
+            'error' => false,
+            'message' => 'Job is running, you will receive an email when it is done.',
+            ], 200);
+    }
+
 
 }
